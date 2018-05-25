9fac9f1889ace7694bedcdfd9fdde39097ff92a3
unknown <sj1914@WIN.DOC.IC.AC.UK>
2018-05-25 14:21:13 +0100

    Trying to make it run on double click

--- execute_python.bat
+++ execute_python.bat
@@ -1,11 +1,14 @@
-conda remove --name menpo --all
-conda create --name menpo --file spec-file.txt
-echo 'Starting script...' > logfile.log
+conda create --name menpo --file spec-file.txt && (
+  echo "Creating python environment" > logfile.log
+) || (
+  echo "Created python environment" > logfile.log
+)
+
+echo "Activating environment" > logfile.log
 call activate menpo > logfile.log
-echo 'Environment Activated...' > logfile.log
+echo "Launching graphical user interface" > logfile.log
 python gui.py > logfile.log
-echo 'Python Script Run...' > logfile.log
+echo "Deactivating environment" > logfile.log
 call deactivate > logfile.log
-echo 'Environment Deactivated.' > logfile.log
-echo 'Preparing for application launch...' > logfile.log
+echo "Preparing to launch customiser" > logfile.log
 start App\Customiser.exe
